7c6db6b5b85db6f6fcb140c3a172a7ecd4e6e5ef
Josh Whitman <joshtwhitman@gmail.com>
2014-05-03 12:40:26 -0400

    blanked index, updated 404 for favicons
    Blanked out the placeholder body content on index, added favicons to
    the 404 page.

--- index.php
+++ index.php
@@ -1,139 +1,15 @@
 <?php
-$PageTitle="joshwhitman.net";
+	$PageTitle="joshwhitman.net";
 
-function customPageHeader(){?>
-<!--insert additional header content here-->
+	function customPageHeader(){?>
+		<!--insert additional header content here-->
 <?php }
-include('header.php');
+	include('header.php');
 ?>
-        <div class="sidebar">
-			<img class="sidebar" src="img/hex_logo.png" alt="logo" />
-			<h1 class="sidebar">josh<strong>whitman</strong></h1>
-			<p class="sidebar"><strong>QA analyst</strong> and <strong>web developer</strong> with experience working in marketing, education, and technology/media. He also likes cartoons and likes to write!</p>
-		</div>
-		<main class="main">
-			<section id="about">
-				<img class="main" src="img/profile_image.png" alt="profile image" />
-				<h1>about</h1>
-				<p>I am a QA analyst and digital developer with experience working in marketing, education, and technology/media.</p>
-				<p>I value communication highly, in seeing a project to completion, in conveying issues and bugs, in educating on risks and time requirements, and in developing ideas.</p>
-				<p>In my spare time, I pursue creative projects and gain the skills necessary to complete them. These include writing fiction and nonfiction, creating websites with friends, drawing, and various craft projects. I enjoy creating new ways to introduce fun things into my life.</p>
-				<hr />
-			</section>
-			<section id="experience">
-				<h1>experience</h1>
-			<article id="mgb">
-				<h2>QA Analyst at mcgarrybowen</h2>
-				<h4 class="date">May 2012 &mdash; Present (Two Years)</h3>
-				
-				<p>mcgarrybowen is a marketing agency whose digital development team handles work from a wide variety of clients including Verizon, JPMorgan Chase, United Airlines, Kraft, Disney Parks, Chevron, Northrop Grumman, and others. As a QA analyst on this team, I performed the role of manual tester on all work passing through our department.
-						</p>
-				<h3 id="mgb_responsibilities">Responsibilities</h3>
-				<h4>Tested:</h4>
-				<table>
-					<tbody>
-					<tr>
-						<td>Responsive websites</td>
-						<td>Microsites</td>
-					<tr>
-						<td>Emails and newsletters</td>
-						<td>Custom CMS platforms</td>
-						
-					</tr>
-					<tr>
-						<td>Mobile apps</td>
-						<td>Mobile sites</td>
-					</tr>
-					<tr>
-						<td>Digital magazines</td>
-						<td>Accessibility compliance</td>
-					</tr>
-					</tr>
-						<td>Banner ads:<hr />
-							<ul>
-								<li>HTML5</li>
-								<li>Standard Flash</li>
-								<li>Rich Media Flash</li>
-						</ul>
-						</td>
-						<td>Proprietary CMS including:<hr />
-							<ul>
-								<li>Wordpress</li>
-								<li>Tumblr</li>
-								<li>Drupal</li>
-							</ul>
-						</td>
-					</tr>
-				</tbody>
-				</table>
-				<h4>Tasks</h4>
-				<ul>
-					<li>Handled multiple projects daily.</li>
-					<li>Collaborated with producers to see projects through to completion.</li>
-					<li>Set expectations, enforced schedules, and clarified risks to creative, account, and production teams.</li>
-					<li>Created and executed test plans.</li>
-					<li>Administrated our defect tracking platform, Jira.</li>
-					<ul>
-					<li>Includes validation of all issues and tasks, organizing and creating projects, and user management.</li>
-				</ul>
-					<li>Provided allocation estimates for projects.</li>
-					<li>Responsible for ensuring W3C accessibility guideline compliance where required.</li>
-				</ul>
-				<h3 id="mgb_accomplishments">Accomplishments</h3>
-				<ul>
-					<li>Created documentation for banner ad testing for off-shore analysts.</li>
-					<li>Created documentation and conducted presentations for account, creative, and prodution teams to utilize issue tracking software effectively.</li>
-					<li>Improved issue tracking workflow to make validations more efficient.</li>
-					<li>Acted as interim QA manager on a substitute basis.</li>
-					<li>Learned automation testing skills (Selenium, Python).</li>
-				</ul>
-				</article>
-				<hr />
-				
-			<article id="mgb">
-				<h2>Creative Director and Managing&nbsp;Editor at Bosskey.org</h2>
-				<h4 class="date">April 2011 &mdash; March 2014 (Three Years)</h3>
-				
-				<p>Bosskey is a blog and I made it yay</p>
-				<h3 id="mgb_responsibilities">Responsibilities</h3>
-				<ul>
-					<li>Handled multiple projects daily.</li>
-					<li>Collaborated with producers to see projects through to completion.</li>
-					<li>Set expectations, enforced schedules, and clarified risks to creative, account, and production teams.</li>
-					<li>Created and executed test plans.</li>
-					<li>Administrated our defect tracking platform, Jira.</li>
-					<ul>
-					<li>Includes validation of all issues and tasks, organizing and creating projects, and user management.</li>
-				</ul>
-					<li>Provided allocation estimates for projects.</li>
-					<li>Responsible for ensuring W3C accessibility guideline compliance where required.</li>
-				</ul>
-				<h3 id="mgb_accomplishments">Accomplishments</h3>
-				<ul>
-					<li>Created documentation for banner ad testing for off-shore analysts.</li>
-					<li>Created documentation and conducted presentations for account, creative, and prodution teams to utilize issue tracking software effectively.</li>
-					<li>Improved issue tracking workflow to make validations more efficient.</li>
-					<li>Acted as interim QA manager on a substitute basis.</li>
-					<li>Learned automation testing skills (Selenium, Python).</li>
-				</ul>
-				</article>
-			</section>
 
-		</main>
-			<footer>
-				<ul>
-					<li>
-						<a href="http://joshura.com" alt="">Tumblr</a>
-					</li>
-					<li>
-						<a href="http://linkedin.com/in/joshura/" alt="">LinkedIn</a>
-					</li>
-				</ul>
-			</footer>
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
-        <script src="js/plugins.js"></script>
-        <script src="js/main.js"></script>
-	<?php
+
+
+		
+<?php
 	include('footer.php');		
-	?>
+?>
