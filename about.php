4f5936347bfd2edd1a65fa4705ad16cf7fc50b6e
Josh Whitman <joshtwhitman@gmail.com>
2014-05-17 16:14:33 -0400

    gave the resume some real content
    re-worked the experience section to take skill-oriented stuff out, to
    be reinserted in a skill-specific section later.

--- about.php
+++ about.php
@@ -15,100 +15,123 @@ include('header.php');
 			<section id="about">
 				<img class="main" src="img/profile_image.png" alt="profile image" />
 				<h1>about</h1>
-				<p>I am a QA analyst and digital developer with experience working in marketing, education, and technology/media.</p>
-				<p>I value communication highly, in seeing a project to completion, in conveying issues and bugs, in educating on risks and time requirements, and in developing ideas.</p>
-				<p>In my spare time, I pursue creative projects and gain the skills necessary to complete them. These include writing fiction and nonfiction, creating websites with friends, drawing, and various craft projects. I enjoy creating new ways to introduce fun things into my life.</p>
+				<p>Hey there!</p>
+				<p>I’m Josh, and I’m a QA analyst and digital developer with experience working in marketing, education, and technology/media.</p>
+				<p>Thanks for taking the time to check out my site! The page you’re reading is an inventory of the skills and experience I’ve gained through the years in pursuit of my ever-changing Big Picture. One might call it a résumé.</p>
+				<p>My Big Picture is about channeling my desire to create, improve, and build. As a student, I pursued liberal arts to the nth degree to gain as much knowledge as interested me. As a teacher, I spent a lot of my time improving processes and increasing the capabilities of my co-workers and students. As a designer, writer, and developer, I put ideas into action, seeking new skills whenever they become necessary. As a QA analyst, I follow projects to their audiences and help perfect the experience therein. </p>
+				<p>In my spare time, I pursue creative projects and gain the skills necessary to complete them. These include writing fiction and nonfiction, creating websites with and for friends, drawing, and various craft projects (next, I’m building a computer inside of a Gamecube). I enjoy creating new ways to introduce fun things into my life.</p>
+				<p>&mdash; Josh</p>
 				<hr />
 			</section>
+			<!--<h4>Tested:</h4>
+				<div class="table_list">
+				<ul>
+				<li>Responsive websites</li>
+				<li>Microsites</li>
+				<li>Emails and newsletters</li>
+				<li>Custom CMS platforms</li>
+				<li>Mobile apps</li>
+				<li>Mobile sites</li>
+				<li>Digital magazines</li>
+				<li>Accessibility compliance</li>
+				<li>Banner ads:
+				<ul>
+				<li>HTML5</li>
+				<li>Standard Flash</li>
+				<li>Rich Media Flash</li>
+				</ul>
+				</li>
+				<li>Proprietary CMS including:
+				<ul>
+				<li>Wordpress</li>
+				<li>Tumblr</li>
+				<li>Drupal</li>
+				</ul>
+				</li>
+				</ul>
+				</div>-->
 			<section id="experience">
 				<h1>experience</h1>
-			<article id="mgb">
-				<h2>QA Analyst at mcgarrybowen</h2>
-				<h4 class="date">May 2012 &mdash; Present (Two Years)</h3>
-				
-				<p>mcgarrybowen is a marketing agency whose digital development team handles work from a wide variety of clients including Verizon, JPMorgan Chase, United Airlines, Kraft, Disney Parks, Chevron, Northrop Grumman, and others. As a QA analyst on this team, I performed the role of manual tester on all work passing through our department.
-						</p>
-				<h3 id="mgb_responsibilities">Responsibilities</h3>
-				<h4>Tested:</h4>
-				<div class="table_list">
+				<article id="mgb">
+					<h2>QA Analyst at mcgarrybowen</h2>
+					<h4 class="date">May 2012 &mdash; Present</h4>
+					<p>mcgarrybowen is a marketing agency whose digital development team handles work from a wide variety of clients including Verizon, JPMorgan Chase, United Airlines, Kraft, Disney Parks, Chevron, Northrop Grumman, and others.</p>
+					<h4>Duties performed:</h4>
 					<ul>
-						<li>Responsive websites</li>
-						<li>Microsites</li>
-						<li>Emails and newsletters</li>
-						<li>Custom CMS platforms</li>
-						<li>Mobile apps</li>
-						<li>Mobile sites</li>
-						<li>Digital magazines</li>
-						<li>Accessibility compliance</li>
-						<li>Banner ads:
-							<ul>
-								<li>HTML5</li>
-								<li>Standard Flash</li>
-								<li>Rich Media Flash</li>
-							</ul>
-						</li>
-						<li>Proprietary CMS including:
-							<ul>
-								<li>Wordpress</li>
-								<li>Tumblr</li>
-								<li>Drupal</li>
-							</ul>
-						</li>
+						<li>Acted as primary tester for a wide range of sites and apps, including the new Central Park Conservancy mobile app (for iOS and Android), mcgarrybowen’s internal websites, several iterations of Verizon’s DroidDoes.com and Powerful Answers sites, Purina Beyond Pet Food website, Lunchables Uploaded site, and Marriott’s Residence Inn website.</li>
+						<li>Improved validation process in Jira-based issue tracking workflow.</li>
+						<li>Taught three new members of the QA team and created documentation on our team’s procedures for banner testing, email testing, issue tracking, and other general best practices.</li>
+						<li>Oversaw implementation of and transition to AtTask for project management and issue tracking.</li>
+						<li>Taught developers and producers and created documentation on compliance with the W3C standards for accessible websites.</li>
+						<li>Conducted presentations and created documentation for account, creative, and production teams on utilizing QA and issue tracking systems effectively.</li>
+						<li>Managed QA department allocations, scheduling, risk assessment, and project planning on a frequent  basis.</li>
+						<li>Introduced automation options using Selenium and Python for web applications.</li>
 					</ul>
-				</div>
-				<h4>Tasks</h4>
-				<ul>
-					<li>Handled multiple projects daily.</li>
-					<li>Collaborated with producers to see projects through to completion.</li>
-					<li>Set expectations, enforced schedules, and clarified risks to creative, account, and production teams.</li>
-					<li>Created and executed test plans.</li>
-					<li>Administrated our defect tracking platform, Jira.</li>
+				</article>
+				<hr />	
+				<article id="bosskey">
+					<h2>Creative Director and Managing&nbsp;Editor at Bosskey.org</h2>
+					<h4 class="date">April 2011 &mdash; March 2014</h4>
+					<p>Bosskey is a gaming, comics, and general nerd-themed blog and forum community built on the CodeIgniter framework by a two-person team.</p>
+					<h4>Duties performed:</h4>
 					<ul>
-					<li>Includes validation of all issues and tasks, organizing and creating projects, and user management.</li>
-				</ul>
-					<li>Provided allocation estimates for projects.</li>
-					<li>Responsible for ensuring W3C accessibility guideline compliance where required.</li>
-				</ul>
-				<h3 id="mgb_accomplishments">Accomplishments</h3>
-				<ul>
-					<li>Created documentation for banner ad testing for off-shore analysts.</li>
-					<li>Created documentation and conducted presentations for account, creative, and prodution teams to utilize issue tracking software effectively.</li>
-					<li>Improved issue tracking workflow to make validations more efficient.</li>
-					<li>Acted as interim QA manager on a substitute basis.</li>
-					<li>Learned automation testing skills (Selenium, Python).</li>
-				</ul>
+						<li>Created, designed, planned, and promoted website, working closely with a developer by providing detailed comps, wireframes, direction, and collaborating on data models, roadmaps, and feature planning.</li>
+						<li>Handled all QA by combining white-box, black-box, and user acceptance testing. Tracked defects and  acceptance criteria using Mantis Bug Tracker.</li>
+						<li>Gathered user and author feedback during beta and post-launch to generate and prioritize new features and improvements.</li>
+						<li>Researched, wrote, and published original articles.</li>
+						<li>Managed a team of writers and editors creating their own articles, keeping content updates to a consistent schedule.</li>
+						<li>Edited copy and fact-checked new content.</li>
+						<li>Engaged social media to reach hundreds of people with new posts.</li>
+					</ul>
 				</article>
 				<hr />
-				
-			<article id="mgb">
-				<h2>Creative Director and Managing&nbsp;Editor at Bosskey.org</h2>
-				<h4 class="date">April 2011 &mdash; March 2014 (Three Years)</h3>
-				
-				<p>Bosskey is a blog and I made it yay</p>
-				<h3 id="mgb_responsibilities">Responsibilities</h3>
-				<ul>
-					<li>Handled multiple projects daily.</li>
-					<li>Collaborated with producers to see projects through to completion.</li>
-					<li>Set expectations, enforced schedules, and clarified risks to creative, account, and production teams.</li>
-					<li>Created and executed test plans.</li>
-					<li>Administrated our defect tracking platform, Jira.</li>
+				<article id="heart">
+					<h2>Assistant Language Teacher with Heart School in Ibaraki, Japan</h2>
+					<h4 class="date">September 2010 &mdash; April 2012</h4>
+					<p>Heart School is an English teaching company basd in Mito, Ibaraki, in Japan, that hires native English speakers for dispatch positions teaching in various school systems in its regions.</p>
+					<h4>Duties performed:</h4>
 					<ul>
-					<li>Includes validation of all issues and tasks, organizing and creating projects, and user management.</li>
-				</ul>
-					<li>Provided allocation estimates for projects.</li>
-					<li>Responsible for ensuring W3C accessibility guideline compliance where required.</li>
-				</ul>
-				<h3 id="mgb_accomplishments">Accomplishments</h3>
-				<ul>
-					<li>Created documentation for banner ad testing for off-shore analysts.</li>
-					<li>Created documentation and conducted presentations for account, creative, and prodution teams to utilize issue tracking software effectively.</li>
-					<li>Improved issue tracking workflow to make validations more efficient.</li>
-					<li>Acted as interim QA manager on a substitute basis.</li>
-					<li>Learned automation testing skills (Selenium, Python).</li>
-				</ul>
+						<li>Taught English to middle school students ages 12-15 at Fuchu Jr. High School in Ishioka.</li>
+						<li>Worked with teachers to create and conduct lessons for beginner to intermediate students.</li>
+						<li>Provided tutoring to students requiring additional support.</li>
+						<li>Assisted teachers in club leadership for Computer and Kendo clubs.</li>
+						<li>Organized and hosted after school conversation groups for students looking for additional practice.</li>
+					</ul>
+				</article>
+				<hr />
+				<article id="tia">
+					<h2>Part-time Language Teacher at Tsukuba International Academy in Tsuchiura, Japan</h2>
+					<h4 class="date">January 2011 &mdash; March 2012</h4>
+					<p>Tsukuba International Academy is an English conversation school that provides extracurricular language tutoring and instruction to private parties and businesses, with no age restrictions.</p>
+					<h4>Duties performed:</h4>
+					<ul>
+						<li>Taught beginner English to kindergarten students, advanced English to business-sponsored students, intermediate English to high school students, and very advanced English to one awesome 9-year old.</li>
+						<li>Created games and lesson presentation material for use by all teachers.</li>
+						<li>Taught new teachers job responsibilities and procedures.</li>
+					</ul>
+				</article>
+				<hr />
+			</section>
+			<section id="education">
+				<h1>education</h1>
+				<br />
+				<article id="general_courses">
+					<h4>General courses</h4>
+					<ul>
+						<li>Completed Codecademy courses for HTML & CSS, Javascript, jQuery, and Python.</li>
+						<li>Completed training course at RTTS: Intro to Selenium (Dec 2013).</li>
+					</ul>
+				</article>
+				<article id="vassar">
+					<h2>Vassar College, <span>Poughkeepsie, NY</span></h2>
+					<h4 class="date">Bachelor of Arts, Japanese; Class of 2010</h4>
+					<ul>
+						<li>Completed extensive coursework in Computer Science and Media Studies.</li>
+						<li>Student Body liaison for No Such Organization (comics, gaming, fantasy and scifi student group).</li>
+						<li>Volunteer coordinator and publicity chair for No Such Convention 2010.</li>
+					</ul>
 				</article>
 			</section>
-
 		</main>
 			<footer>
 				<ul>
