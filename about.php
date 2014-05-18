6cd78ee9ee1b0a23f49107a4e221e101e05b30fd
Josh Whitman <joshtwhitman@gmail.com>
2014-05-18 18:36:26 -0400

    added skills, masonry for list tables
    Added skills and converted my table lists into masonry tiles. Currently
    debugging something that I need to commit and upload before I can show
    it to people who can help!

--- about.php
+++ about.php
@@ -11,56 +11,109 @@ include('header.php');
 			<h1 class="sidebar">Josh<span>&nbsp;</span><strong>Whitman</strong></h1>
 			<p class="sidebar"><strong>QA analyst</strong> and <strong>web developer</strong> with experience working in marketing, education, and technology/media. He&nbsp;also likes cartoons and likes to write!</p>
 		</div>
+		
 		<main class="main">
+			<nav>
+				<ul>
+					<a href="#about"><li>about</li></a>
+					<a href="#skills"><li>skills</li></a>
+					<a href="#experience"><li>experience</li></a>
+					<a href="#education"><li>education</li></a>
+				</ul>
+			</nav>
 			<section id="about">
 				<img class="main" src="img/profile_image.png" alt="profile image" />
+
 				<h1>about</h1>
-				<p>Hey there!</p>
-				<p>I’m Josh, and I’m a QA analyst and digital developer with experience working in marketing, education, and technology/media.</p>
-				<p>Thanks for taking the time to check out my site! The page you’re reading is an inventory of the skills and experience I’ve gained through the years in pursuit of my ever-changing Big Picture. One might call it a résumé.</p>
-				<p>My Big Picture is about channeling my desire to create, improve, and build. As a student, I pursued liberal arts to the nth degree to gain as much knowledge as interested me. As a teacher, I spent a lot of my time improving processes and increasing the capabilities of my co-workers and students. As a designer, writer, and developer, I put ideas into action, seeking new skills whenever they become necessary. As a QA analyst, I follow projects to their audiences and help perfect the experience therein. </p>
-				<p>In my spare time, I pursue creative projects and gain the skills necessary to complete them. These include writing fiction and nonfiction, creating websites with and for friends, drawing, and various craft projects (next, I’m building a computer inside of a Gamecube). I enjoy creating new ways to introduce fun things into my life.</p>
-				<p>&mdash; Josh</p>
-				<hr />
+				<article>
+					<p>Hey there!</p>
+					<p>I&rsquo;m Josh, and I&rsquo;m a <strong>QA analyst</strong> and <strong>digital developer</strong> with experience working in marketing, education, and technology/media. Thanks for taking the time to check out my site!</p>
+					<p>I have worn many hats through the years, and I consider each as valuable as the last. As a <strong>student</strong>, I pursued liberal arts to the nth degree to gain as much knowledge as interested me. As a <strong>teacher</strong>, I spent a lot of my time improving processes and increasing the capabilities of my co-workers and students. As a <strong>designer</strong>, <strong>writer</strong>, and <strong>developer</strong>, I put ideas into action, seeking new skills whenever they become necessary. As a <strong>QA analyst</strong>, I follow projects to their audiences and help perfect the experience therein.</p>
+					<p>In my spare time, I pursue creative projects and gain the skills necessary to complete them. These include writing fiction and nonfiction, creating websites with and for friends, drawing, and various craft projects (next, I&rsquo;m building a computer inside of a Gamecube). I enjoy creating new ways to introduce fun things into my life.</p>
+					<h3>&mdash; <span>Josh</span></h3>
+				</article>
+				<!--<hr />-->
 			</section>
-			<!--<h4>Tested:</h4>
-				<div class="table_list">
-				<ul>
-				<li>Responsive websites</li>
-				<li>Microsites</li>
-				<li>Emails and newsletters</li>
-				<li>Custom CMS platforms</li>
-				<li>Mobile apps</li>
-				<li>Mobile sites</li>
-				<li>Digital magazines</li>
-				<li>Accessibility compliance</li>
-				<li>Banner ads:
-				<ul>
-				<li>HTML5</li>
-				<li>Standard Flash</li>
-				<li>Rich Media Flash</li>
-				</ul>
-				</li>
-				<li>Proprietary CMS including:
-				<ul>
-				<li>Wordpress</li>
-				<li>Tumblr</li>
-				<li>Drupal</li>
-				</ul>
-				</li>
-				</ul>
-				</div>-->
+			<section id="skills">
+				<h1>skills</h1>
+				<article id="general">
+					<h2>General</h2>
+					<div class="table_list">
+						<ul>
+							<li>Writing and communication</li>
+							<li>Building documentation</li>
+							<li>Project management</li>
+							<li>Community management</li>
+							<li>Time management and estimates</li>
+							<li>Teaching</li>
+						</ul>
+					</div>
+				</article>
+				<article id="qa">
+					<h2>QA Analyst</h2>
+					<div class="table_list">
+						<ul>
+							<li><strong>Black-box testing</strong>:
+								<ul>
+									<li>Responsive websites</li>
+									<li>Emails and newsletters</li>
+									<li>Mobile apps</li>
+									<li>Digital magazines</li>
+									<li>Accessibility compliance</li>
+									<li>Rich Media and Standard <strong>Banner ads</strong> in HTML5 and Flash
+									<!--	<ul>
+											<li>HTML5</li>
+											<li>Standard Flash</li>
+											<li>Rich Media Flash</li>
+										</ul>-->
+									</li>
+								</ul>
+							</li>
+							<li><strong>White-box testing</strong> for mobile apps and CMS implementations</li>
+							<li>Bug tracking software including <strong>Jira</strong>, <strong>Mantis</strong>, and <strong>Bugzilla</strong></li>
+							<li>Tools including <strong>Charles</strong>, <strong>Wireshark</strong>, <strong>Firebug</strong>, and dev tools for all modern browsers</li>
+							<li>Accessibility testing for <strong>W3C</strong> guideline compliance</li>
+							<li>Test case building from comps or wireframes</li>
+							<li>Automated testing of web applications with <strong>Selenium</strong></li>
+						</ul>
+					</div>
+				</article>
+				<article id="developer">
+					<h2>Developer</h2>
+					<div class="table_list">
+						<ul>
+							<li>Front-end technologies including <strong>HTML5</strong>, <strong>CSS3</strong>, <strong>Javascript</strong>, and <strong>jQuery</strong></li>
+							<li>Languages including <strong>PHP</strong>, <strong>Java</strong>, and <strong>Python</strong></li>
+							<li>Responsive, mobile-first websites</li>
+							<li>Implementation of CMS softwares including <strong>Drupal</strong>, <strong>Wordpress</strong>, <strong>Joomla</strong>, and <strong>Tumblr</strong></li>
+							<li>Accessible coding best practices</li>
+						</ul>
+					</div>
+				</article>
+				<article id="designer">
+					<h2>Designer and UX</h2>
+					<div class="table_list">
+						<ul>
+							<li>Adobe Suite including <strong>Photoshop</strong>, <strong>Illustrator</strong>, and <strong>Flash</strong></li>
+							<li>Creation of compositions for complex web applications</li>
+							<li>Accessible design best practices</li>
+							<li>Wireframing</li>
+						</ul>
+					</div>
+				</article>
+				<!--<hr />-->
+			</section>
 			<section id="experience">
 				<h1>experience</h1>
 				<article id="mgb">
 					<h2>QA Analyst at mcgarrybowen</h2>
-					<h4 class="date">May 2012 &mdash; Present</h4>
+					<h4 class="date">May 2012 &mdash; Present; New&nbsp;York,&nbsp;NY</h4>
 					<p>mcgarrybowen is a marketing agency whose digital development team handles work from a wide variety of clients including Verizon, JPMorgan Chase, United Airlines, Kraft, Disney Parks, Chevron, Northrop Grumman, and others.</p>
-					<h4>Duties performed:</h4>
+					<h4>Accomplishments:</h4>
 					<ul>
-						<li>Acted as primary tester for a wide range of sites and apps, including the new Central Park Conservancy mobile app (for iOS and Android), mcgarrybowen’s internal websites, several iterations of Verizon’s DroidDoes.com and Powerful Answers sites, Purina Beyond Pet Food website, Lunchables Uploaded site, and Marriott’s Residence Inn website.</li>
+						<li>Acted as primary tester for a wide range of sites and apps, including the new Central Park Conservancy mobile app (for iOS and Android), mcgarrybowen&rsquo;s internal websites, several iterations of Verizon&rsquo;s DroidDoes.com and Powerful Answers sites, Purina Beyond Pet Food website, Lunchables Uploaded site, and Marriott&rsquo;s Residence Inn website.</li>
 						<li>Improved validation process in Jira-based issue tracking workflow.</li>
-						<li>Taught three new members of the QA team and created documentation on our team’s procedures for banner testing, email testing, issue tracking, and other general best practices.</li>
+						<li>Taught three new members of the QA team and created documentation on our team&rsquo;s procedures for banner testing, email testing, issue tracking, and other general best practices.</li>
 						<li>Oversaw implementation of and transition to AtTask for project management and issue tracking.</li>
 						<li>Taught developers and producers and created documentation on compliance with the W3C standards for accessible websites.</li>
 						<li>Conducted presentations and created documentation for account, creative, and production teams on utilizing QA and issue tracking systems effectively.</li>
@@ -68,12 +121,12 @@ include('header.php');
 						<li>Introduced automation options using Selenium and Python for web applications.</li>
 					</ul>
 				</article>
-				<hr />	
+				<!--<hr />-->	
 				<article id="bosskey">
 					<h2>Creative Director and Managing&nbsp;Editor at Bosskey.org</h2>
 					<h4 class="date">April 2011 &mdash; March 2014</h4>
 					<p>Bosskey is a gaming, comics, and general nerd-themed blog and forum community built on the CodeIgniter framework by a two-person team.</p>
-					<h4>Duties performed:</h4>
+					<h4>Accomplishments:</h4>
 					<ul>
 						<li>Created, designed, planned, and promoted website, working closely with a developer by providing detailed comps, wireframes, direction, and collaborating on data models, roadmaps, and feature planning.</li>
 						<li>Handled all QA by combining white-box, black-box, and user acceptance testing. Tracked defects and  acceptance criteria using Mantis Bug Tracker.</li>
@@ -84,12 +137,12 @@ include('header.php');
 						<li>Engaged social media to reach hundreds of people with new posts.</li>
 					</ul>
 				</article>
-				<hr />
+				<!--<hr />-->
 				<article id="heart">
-					<h2>Assistant Language Teacher with Heart School in Ibaraki, Japan</h2>
-					<h4 class="date">September 2010 &mdash; April 2012</h4>
-					<p>Heart School is an English teaching company basd in Mito, Ibaraki, in Japan, that hires native English speakers for dispatch positions teaching in various school systems in its regions.</p>
-					<h4>Duties performed:</h4>
+					<h2>Assistant Language Teacher with Heart&nbsp;School</h2>
+					<h4 class="date">September 2010 &mdash; April 2012; Ibaraki,&nbsp;Japan</h4>
+					<p>Heart School is an English teaching company basd in Mito,&nbsp;Ibaraki, in Japan, that hires native English speakers for dispatch positions teaching in various school systems in its regions.</p>
+					<h4>Accomplishments:</h4>
 					<ul>
 						<li>Taught English to middle school students ages 12-15 at Fuchu Jr. High School in Ishioka.</li>
 						<li>Worked with teachers to create and conduct lessons for beginner to intermediate students.</li>
@@ -98,19 +151,19 @@ include('header.php');
 						<li>Organized and hosted after school conversation groups for students looking for additional practice.</li>
 					</ul>
 				</article>
-				<hr />
+				<!--<hr />-->
 				<article id="tia">
-					<h2>Part-time Language Teacher at Tsukuba International Academy in Tsuchiura, Japan</h2>
-					<h4 class="date">January 2011 &mdash; March 2012</h4>
+					<h2>Language Teacher at Tsukuba International Academy</h2>
+					<h4 class="date">January 2011 &mdash; March 2012; Tsuchiura,&nbsp;Japan</h4>
 					<p>Tsukuba International Academy is an English conversation school that provides extracurricular language tutoring and instruction to private parties and businesses, with no age restrictions.</p>
-					<h4>Duties performed:</h4>
+					<h4>Accomplishments:</h4>
 					<ul>
 						<li>Taught beginner English to kindergarten students, advanced English to business-sponsored students, intermediate English to high school students, and very advanced English to one awesome 9-year old.</li>
 						<li>Created games and lesson presentation material for use by all teachers.</li>
 						<li>Taught new teachers job responsibilities and procedures.</li>
 					</ul>
 				</article>
-				<hr />
+				<!--<hr />-->
 			</section>
 			<section id="education">
 				<h1>education</h1>
@@ -123,8 +176,8 @@ include('header.php');
 					</ul>
 				</article>
 				<article id="vassar">
-					<h2>Vassar College, <span>Poughkeepsie, NY</span></h2>
-					<h4 class="date">Bachelor of Arts, Japanese; Class of 2010</h4>
+					<h2>Vassar&nbsp;College, <span>Poughkeepsie,&nbsp;NY</span></h2>
+					<h4 class="date">Bachelor of Arts, Japanese; Class&nbsp;of&nbsp;2010</h4>
 					<ul>
 						<li>Completed extensive coursework in Computer Science and Media Studies.</li>
 						<li>Student Body liaison for No Such Organization (comics, gaming, fantasy and scifi student group).</li>
